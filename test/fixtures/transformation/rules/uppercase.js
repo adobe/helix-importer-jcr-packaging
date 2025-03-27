24382069764afcd371e2b0b3d5b9cb4aea15e464
Ben Helleman <bhellema@adobe.com>
2025-03-27 13:54:12 -0400

    Revert "fix(sites-30112): support transforming model field values (#10)"
    This reverts commit 082c84489dff628c4c51b02af2ab11de92cc50b8. But keeps some changes in packaging.js

--- test/fixtures/transformation/rules/uppercase.js
+++
@@ -1,9 +0,0 @@
-/**
- * Transforms text to uppercase
- * @param {string} value - The input value to transform
- * @returns {string} The transformed value in uppercase
- */
-export default function uppercase(value) {
-  if (!value) return '';
-  return value.toString().toUpperCase();
-}
